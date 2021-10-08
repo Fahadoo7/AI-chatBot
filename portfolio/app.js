7c023707cc8ef0c82f1ae2cb0d71bbae4e17fd75
Syed Fahad <57718052+Fahadoo7@users.noreply.github.com>
2021-10-08 13:04:34 +0500

    Add files via upload

---
+++ portfolio/app.js
@@ -0,0 +1,10 @@
+function change()
+{
+ var bc =document.getElementsByTagName('body')[0];
+    if(bc.className=='bg-white'){
+     bc.className='bg-dark clw';
+    }else{
+     bc.className='bg-white';
+    }
+
+}
